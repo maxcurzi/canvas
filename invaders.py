cc7d8ddf7f3a4ef1cbf45f5f7eb4a1bfc6712c92
Max Curzi <massimiliano.curzi@gmail.com>
2023-01-17 17:43:06 +0000

    add space invaders clone

---
+++ invaders.py
@@ -0,0 +1,302 @@
+import pygame
+from enum import Enum, auto
+from PIL import Image
+import random
+
+
+class Team(Enum):
+    HUMANS = auto()
+    ALIENS = auto()
+    SHIELDS = auto()
+
+
+class Game:
+    def __init__(self, width: int, height: int) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        self.player: Game.Human = Game.Human((0, 255, 0), 1, 56)
+        self.aliens: pygame.sprite.Group = pygame.sprite.Group()
+        self.shields: pygame.sprite.Group = pygame.sprite.Group()
+        self.all_sprites: pygame.sprite.Group = pygame.sprite.Group()
+        self.player_rockets: pygame.sprite.Group = pygame.sprite.Group()
+        self.enemies_rockets: pygame.sprite.Group = pygame.sprite.Group()
+        self.resolution = (width, height)
+        self.clock = pygame.time.Clock()
+        self.all_sprites.add(self.player)
+
+    def _draw_aliens(self):
+        for alien in self.aliens:
+            self.screen.blit(alien.surf, alien.rect)
+        pass
+
+    def _draw_player(self):
+        self.screen.blit(self.player.surf, self.player.rect)
+
+    def _draw_shields(self):
+        pass
+
+    def winner(self):
+        if len(self.aliens) == 0:
+            return Team.HUMANS
+        if self.player.health <= 0:
+            return Team.ALIENS
+        return None
+
+    def draw(self):
+        # Draw all sprites
+        self.screen.fill((0, 0, 0))
+        for entity in self.all_sprites:
+            self.screen.blit(entity.surf, entity.rect)
+            if isinstance(entity, Game.Alien) or isinstance(entity, Game.Human):
+                # Draw health bar
+                self.screen.fill((255, 0, 255), entity.hb_rect)
+
+    def _handle_collisions(self):
+        for alien in self.aliens:
+            if (
+                len(
+                    pygame.sprite.spritecollide(alien, self.player_rockets, dokill=True)
+                )
+                > 0
+            ):
+                alien.health -= 1
+        for alien in self.aliens:
+            if len(pygame.sprite.spritecollide(alien, self.shields, dokill=True)) > 0:
+                pass
+        for shield in self.shields:
+            if (
+                len(
+                    pygame.sprite.spritecollide(
+                        shield, self.player_rockets, dokill=True
+                    )
+                )
+                > 0
+            ) or (
+                len(
+                    pygame.sprite.spritecollide(
+                        shield, self.enemies_rockets, dokill=True
+                    )
+                )
+                > 0
+            ):
+                shield.health -= 1
+        if (
+            len(
+                pygame.sprite.spritecollide(
+                    self.player, self.enemies_rockets, dokill=True
+                )
+            )
+            > 0
+        ):
+            self.player.health -= 1
+        if len(pygame.sprite.spritecollide(self.player, self.aliens, dokill=True)) > 0:
+            self.player.health = 0
+
+    def _handle_actions(self):
+        pass
+
+    def update(self, clicked_pixels: list | None = None):
+        self.all_sprites.update()
+        self._handle_collisions()
+        self.draw()
+        window_width, window_height = pygame.display.get_surface().get_size()
+        # pygame.display.set_mode((640, 640))
+        pygame.display.flip()
+        self.clock.tick(60)
+        pass
+
+    class Alien(pygame.sprite.Sprite):
+        # Constructor. Pass in the color of the block,
+        # and its x and y position
+        def __init__(self, color, x0, y0, owner=None):
+            # Call the parent class (Sprite) constructor
+            pygame.sprite.Sprite.__init__(self)
+            self.surf = pygame.image.load("games/invaders/alien.png").convert()
+            # self.surf.fill(color)
+            self.surf.set_colorkey((0, 0, 0))
+            self.rect = self.surf.get_rect()
+            self.x0 = x0
+            self.y0 = y0
+            self.rect.x = x0
+            self.rect.y = y0
+            self.health = 7
+            self.health_bar = pygame.Surface((self.health, 1))
+            self.hb_rect = self.health_bar.get_rect()
+            self.hb_rect.x = x0 + 2
+            self.hb_rect.y = y0 - 2
+            # self.health_bar.fill((255, 0, 255))
+            self.owner = owner
+            self.update_rate = 1
+            self.t = 0
+            self.dx = 1
+
+        def update(self):
+            super().update(self)
+            if self.health <= 0:
+                self.kill()
+            self.t += 1
+            if self.t % self.update_rate == 0:
+                # Move
+                self.rect.x += self.dx
+                self.hb_rect.x += self.dx
+                self.hb_rect.width = max(0, self.health)
+                if (self.dx > 0 and self.rect.x - self.x0 >= 27) or (
+                    self.dx < 0 and self.rect.x - self.x0 <= 0
+                ):
+                    self.dx = -self.dx
+                    self.rect.y += 1
+                    self.hb_rect.y += 1
+
+    class Human(pygame.sprite.Sprite):
+        # Constructor. Pass in the color of the block,
+        # and its x and y position
+        def __init__(self, color, x0, y0, owner=None):
+            # Call the parent class (Sprite) constructor
+            pygame.sprite.Sprite.__init__(self)
+            self.surf = pygame.image.load("games/invaders/human.png").convert()
+            # self.surf.fill(color)
+            self.surf.set_colorkey((0, 0, 0))
+            self.surf.fill((0, 255, 200), special_flags=pygame.BLEND_MULT)
+
+            # Fetch the rectangle object that has the dimensions of the image
+            # Update the position of this object by setting the values of rect.x and rect.y
+            self.rect = self.surf.get_rect()
+            self.rect.x = x0
+            self.rect.y = y0
+            self.health = 11
+            self.health_bar = pygame.Surface((self.health, 1))
+            self.hb_rect = self.health_bar.get_rect()
+            self.hb_rect.x = x0
+            self.hb_rect.y = y0 + 6
+            self.owner = owner
+            self.update_rate = 3
+            self.t = 0
+            self.dx = 1
+
+        def update(self):
+            super().update(self)
+            self.t += 1
+            if self.t % self.update_rate == 0:
+                # Move
+                self.rect.x += self.dx
+                self.hb_rect.x += self.dx
+                self.hb_rect.width = max(0, self.health)
+                if (self.dx > 0 and self.rect.x >= 52) or (
+                    self.dx < 0 and self.rect.x <= 0
+                ):
+                    self.dx = -self.dx
+
+    class Shield(pygame.sprite.Sprite):
+        # Constructor. Pass in the color of the block,
+        # and its x and y position
+        def __init__(self, color, x0, y0, owner=None):
+            # Call the parent class (Sprite) constructor
+            pygame.sprite.Sprite.__init__(self)
+
+            # Create an image of the block, and fill it with a color.
+            # This could also be an image loaded from the disk.
+            self.surf = pygame.Surface([3, 2])
+            self.surf.fill((255, 255, 0))
+            # self.surf.fill(color)
+
+            # Fetch the rectangle object that has the dimensions of the image
+            # Update the position of this object by setting the values of rect.x and rect.y
+            self.rect = self.surf.get_rect()
+            self.rect.x = x0 - 1
+            self.rect.y = y0
+            self.health = 1
+            self.owner = owner
+
+        def update(self):
+            super().update(self)
+            if self.health <= 0:
+                self.kill()
+
+    class Rocket(pygame.sprite.Sprite):
+        # Constructor. Pass in the color of the block,
+        # and its x and y position
+        def __init__(self, color, x0, y0, owner=None, friendly=True):
+            # Call the parent class (Sprite) constructor
+            pygame.sprite.Sprite.__init__(self)
+
+            # Create an image of the block, and fill it with a color.
+            # This could also be an image loaded from the disk.
+            self.surf = pygame.Surface([1, 2])
+            color = (0, 255, 255) if friendly else (255, 0, 0)
+            self.surf.fill(color)
+            self.friendly = friendly
+
+            # Fetch the rectangle object that has the dimensions of the image
+            # Update the position of this object by setting the values of rect.x and rect.y
+            self.rect = self.surf.get_rect()
+            self.rect.x = x0
+            self.rect.y = y0
+            self.health = 1
+            self.owner = owner
+
+        def update(self):
+            super().update(self)
+            self.rect.y = self.rect.y - 1 if self.friendly else self.rect.y + 1
+
+
+def start_game():
+    game = Game(64, 64)
+
+    alien0 = Game.Alien((255, 255, 0), 1, 2)
+    alien1 = Game.Alien((255, 255, 0), 13, 2)
+    alien2 = Game.Alien((255, 255, 0), 25, 2)
+
+    game.aliens.add(alien0)
+    game.aliens.add(alien1)
+    game.aliens.add(alien2)
+
+    game.all_sprites.add(alien0)
+    game.all_sprites.add(alien1)
+    game.all_sprites.add(alien2)
+
+    t = 0
+    while game.winner() == None:
+        game.update()
+        t += 1
+        if t % random.randint(5, 80) == 0:
+            shield = Game.Shield(
+                (255, 0, 255), random.randint(1, 60), random.randint(12, 50)
+            )
+            game.shields.add(shield)
+            game.all_sprites.add(shield)
+        if t % random.randint(10, 80) == 0:
+            rocket = Game.Rocket(
+                (255, 0, 255), game.player.rect.x + 5, game.player.rect.y - 1
+            )
+            game.player_rockets.add(rocket)
+            game.all_sprites.add(rocket)
+
+        if t % random.randint(10, 300) == 0:
+            if alien0.alive():
+                rocket = Game.Rocket(
+                    (255, 0, 0), alien0.rect.x + 5, alien0.rect.y + 5, friendly=False
+                )
+                game.enemies_rockets.add(rocket)
+                game.all_sprites.add(rocket)
+        if t % random.randint(10, 300) == 0:
+            if alien1.alive():
+                rocket = Game.Rocket(
+                    (255, 0, 0), alien1.rect.x + 5, alien1.rect.y + 5, friendly=False
+                )
+                game.enemies_rockets.add(rocket)
+                game.all_sprites.add(rocket)
+        if t % random.randint(10, 300) == 0:
+            if alien2.alive():
+                rocket = Game.Rocket(
+                    (255, 0, 0), alien2.rect.x + 5, alien2.rect.y + 5, friendly=False
+                )
+                game.enemies_rockets.add(rocket)
+                game.all_sprites.add(rocket)
+        im = Image.fromarray(pygame.surfarray.array3d(game.screen)).rotate(-90)
+        im.save("Screenshot.png")
+    print("Winner: " + str(game.winner()))
+
+
+if __name__ == "__main__":
+    while True:
+        start_game()
