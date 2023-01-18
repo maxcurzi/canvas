84674153392394ce2937d94a94ea952f1de92761
Max Curzi <massimiliano.curzi@gmail.com>
2023-01-18 17:09:52 +0000

    Add requirements, setup, and gitignore"

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="canvas-invaders",
+    version="0.0.1",
+    description="Space Invaders Clone for Canvas",
+    author="Max Curzi",
+    author_email="massimiliano.curzi@gmail.com",
+    url="https://github.com/maxcurzi/canvas-game-invaders",
+    # packages=["invaders"],
+)
