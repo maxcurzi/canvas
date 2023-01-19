296b1ceb3c2d9e34ec15f31617442fc0e49620aa
Max Curzi <massimiliano.curzi@gmail.com>
2023-01-19 13:56:04 +0000

    add websafe colors and update grid size to 64

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Grid from './components/Grid';
+import './websafe_colors.css'
 import { firebaseApp, resetGrid } from './Firebase';
 import { useState } from 'react';
 
@@ -11,14 +12,13 @@ import { getPerformance } from "firebase/performance";
 
 function App() {
   const [user, setUser] = useState(null)
-  const gridSize = 48;
+  const gridSize = 64;
   const isAuthenticated = () => {
     return user !== null;
   }
   const auth = getAuth(firebaseApp);
 
   const perf = getPerformance(firebaseApp);
-
   onAuthStateChanged(auth, (user) => {
     if ((user) && (user.emailVerified)) {
         setUser(user)
