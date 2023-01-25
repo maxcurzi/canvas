e167fc8fe5b7585b03b525ede26c51e6c78d5478
Max Curzi <massimiliano.curzi@gmail.com>
2023-01-25 10:15:07 +0000

    restore app

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'react-tooltip/dist/react-tooltip.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppWs />
+    <App />
   </React.StrictMode>
 );
 
