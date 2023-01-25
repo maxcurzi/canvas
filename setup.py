3b741007c6e78007a0fdb6e30920e4a94d6d7c87
Max Curzi <massimiliano.curzi@gmail.com>
2023-01-25 18:10:54 +0000

    add requirements to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="invaders",
-    version="0.0.5",
+    version="0.0.6",
     description="Space Invaders Clone for Canvas",
     author="Max Curzi",
     author_email="massimiliano.curzi@gmail.com",
     url="https://github.com/maxcurzi/canvas-game-invaders",
     package_data={"invaders": ["*.png", "assets/*.png", "**/*.png"]},
     include_package_data=True,
-    install_requires=[],
+    install_requires=requirements,
     packages=find_packages(exclude=["ez_setup", "tests", "tests.*"]),
 )
