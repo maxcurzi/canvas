3126bea6800ce9f6965bddde60bed475608732b2
Max Curzi <massimiliano.curzi@gmail.com>
2023-01-31 20:00:27 +0000

    remove old

--- old/check_req.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "https://35.195.84.144:443/update_pixels"
-myobj = {"x": 30, "y": 30, "user": "cippalippa"}
-session = requests.Session()
-x = requests.post(url, json=myobj, verify=True)
--- old/server.py
+++
@@ -1,8 +0,0 @@
-from old.dbmanager import DbManager
-
-if __name__ == "__main__":
-
-    dbm = DbManager(
-        url="https://canvas-f06e2-default-rtdb.europe-west1.firebasedatabase.app"
-    )
-    dbm.serve_requests()
