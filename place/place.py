14a5d3112fa4648e2e2e94c2e81319ab5f413dc6
Max Curzi <massimiliano.curzi@gmail.com>
2023-03-07 15:22:33 +0000

    linting

--- place/place.py
+++ place/place.py
@@ -1,38 +1,42 @@
 """Monochromatic pixel placement app."""
-from PIL import Image
-import numpy as np
 from enum import Enum
+
+import numpy as np
+from PIL import Image
 
 
 class Colors(Enum):
+    """Colors for the game."""
+
     COLOR_KEY = (0, 0, 0)
     ACTIVE = 96  # (102, 102, 102)
     INACTIVE = 189  # (255, 255, 204)
 
 
 class App:
+    """Monochromatic pixel placement app."""
+
     def __init__(
         self,
         width: int = 64,
         height: int = 64,
     ) -> None:
-        self.resolution = (width, height)
+        self._resolution = (width, height)
         self.pixels = [Colors.INACTIVE] * (width * height)
         self._owners = {}
 
     def update(self):
         """Main game update step, it should be called in a loop to progress the
         game at the game's framerate."""
-        pass
 
-    def click_at(self, x, y, owner=None):
+    def click_at(self, x_pos, y_pos, owner=None):
         """User clicks x,y coordinates and we toggle that pixel's color"""
-        idx = y * self.resolution[0] + x
+        idx = y_pos * self._resolution[0] + x_pos
         pixel_state = self.pixels[idx]
         self.pixels[idx] = (
             Colors.ACTIVE if pixel_state == Colors.INACTIVE else Colors.INACTIVE
         )
-        self._owners[(y, x)] = owner
+        self._owners[(y_pos, x_pos)] = owner
 
     @property
     def owners_map(self):
@@ -41,9 +45,10 @@ class App:
 
     @property
     def frame(self):
+        """Returns current app frame"""
         return Image.fromarray(
             np.array([x.value for x in self.pixels], dtype=np.uint8).reshape(
-                self.resolution
+                self._resolution
             )
         )
 
@@ -51,3 +56,8 @@ class App:
     def finished(self) -> bool:
         """Returns true if the game ended."""
         return False
+
+    @property
+    def resolution(self):
+        """Returns current app resolution in pixels"""
+        return self._resolution
